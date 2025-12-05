795b3535c9b5482adec38aa0de2fea4f4df96255
Oladunjoye jelili olamilekan <jelilioladunjoye04@gmail.com>
2025-12-05 15:51:14 +0100

    commit

--- src/Api/Trending.jsx
+++ src/Api/Trending.jsx
@@ -49,6 +49,7 @@ const Trending = () => {
       { breakpoint: 1600, settings: { slidesToShow: 5 } },
       { breakpoint: 1300, settings: { slidesToShow: 5 } },
       { breakpoint: 992, settings: { slidesToShow: 3 } },
+      { breakpoint: 896, settings: { slidesToShow: 2 } },
       { breakpoint: 768, settings: { slidesToShow: 2 } },
     ],
   };
--- src/Api/UpComing.jsx
+++ src/Api/UpComing.jsx
@@ -49,6 +49,7 @@ const UpComing = () => {
       { breakpoint: 1600, settings: { slidesToShow: 5 } },
       { breakpoint: 1300, settings: { slidesToShow: 4 } },
       { breakpoint: 992, settings: { slidesToShow: 3 } },
+      { breakpoint: 896, settings: { slidesToShow: 2 } },
       { breakpoint: 768, settings: { slidesToShow: 2 } },
     ],
   };
